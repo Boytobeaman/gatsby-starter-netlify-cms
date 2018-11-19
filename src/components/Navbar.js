aa3fe4913d9988510a1f27e48ffe034ca2de65ae
Your Name <you@example.com>
2018-11-20 00:51:46 +0800

    restrap nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,53 +2,70 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown
+} from 'reactstrap';
+export default class Navbar extends React.Component{
+  constructor(props) {
+    super(props);
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link to="/" className="navbar-brand">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-    </Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <Link className="nav-link" to="/">
-            Home
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>
+          <Link to="/" className="navbar-brand">
+                <figure className="image">
+                  <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+                </figure>
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/folding-crates">
-            Folding Crates
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/contact">
-            Contact
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/contact/examples">
-            Form Examples
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </li>
-      </ul>
-      <form className="form-inline my-2 my-lg-0">
-        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav>
-  
-)
-
-export default Navbar
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/folding-crates">
+                Folding Crates
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            <NavItem>
+              <Link className="nav-link" to="/contact/examples">
+                Form Examples
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    )
+  }
+}
