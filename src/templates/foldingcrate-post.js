9ac35515c56d606c2d6b58a6e9439dd4d2900dc6
Your Name <you@example.com>
2018-11-20 22:20:46 +0800

    ui improve

--- src/templates/foldingcrate-post.js
+++ src/templates/foldingcrate-post.js
@@ -77,89 +77,93 @@ export const FoldingcratePostTemplate = ({
             showBullets={true}
           />
         </div>
-        <div className="col-sm-5">
-          <h1 className="title text-capitalize single-product-title h4">
-              {title}
-          </h1>
+        <div className="col-sm-6">
+          <div className="bg-white">
+            <h1 className="title text-capitalize single-product-title h4">
+                {title}
+            </h1>
+            <table className="table table-hover table-bordered single-product-attr">
+              <caption>
+                  <button className="btn btn-danger btn-block product-inquiry">Request a Free Quote</button>
+              </caption>
+              <tbody>
+                  <tr>
+                    <td>
+                      <h6>Product Model</h6>
+                    </td>
+                    <td className="product-model">
+                      <span className="mm pull-left value">{model}</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h6>External Dimensions</h6>
+                      <span>(L * W * H)</span>
+                    </td>
+                    <td className="external-dimension">
+                      <span className="mm pull-left value">{external_long} X {external_width} X {external_height}</span>
+                      <span className="pull-right">mm</span>
+                      <hr className="w-100 mt-4 mb-0" />
+                      <span className="inch pull-left value">{(external_long * mmtoinch).toFixed(2)} X {(external_width * mmtoinch).toFixed(2)} X {(external_height * mmtoinch).toFixed(2)}</span>
+                      <span className="pull-right">in</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h6>Internal Dimensions</h6>
+                      <span>(L * W * H)</span>
+                    </td>
+                    <td className="internal-dimension">
+                      <span className="mm pull-left value">{internal_long} X {internal_width} X {internal_height}</span>
+                      <span className="pull-right">mm</span>
+                      <hr className="w-100 mt-4 mb-0" />
+                      <span className="inch pull-left value">{(internal_long * mmtoinch).toFixed(2)} X {(internal_width * mmtoinch).toFixed(2)} X {(internal_height * mmtoinch).toFixed(2)}</span>
+                      <span className="pull-right">in</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h6>Folded Height</h6>
+                    </td>
+                    <td className="internal-dimension">
+                      <span className="mm pull-left value">{folded_height}</span>
+                      <span className="pull-right">mm</span>
+                      <hr className="w-100 mt-4 mb-0" />
+                      <span className="inch pull-left value">{(folded_height * mmtoinch).toFixed(2)}</span>
+                      <span className="pull-right">in</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h6>Weight</h6>
+                    </td>
+                    <td className="internal-dimension">
+                      <span className="kg pull-left value">{weight}</span>
+                      <span className="pull-right">kg</span>
+                      <hr className="w-100 mt-4 mb-0" />
+                      <span className="lbs pull-left value">{(weight * kgtolbs).toFixed(2)}</span>
+                      <span className="pull-right">lbs</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h6>Volumn</h6>
+                    </td>
+                    <td className="internal-dimension">
+                      <span className="liters pull-left value">{volumn}</span>
+                      <span className="pull-right">Liters</span>
+                      <hr className="w-100 mt-4 mb-0" />
+                      <span className="gallon pull-left value">{(volumn * ltogal).toFixed(2) }</span>
+                      <span className="pull-right">Us gallon</span>
+                    </td>
+                  </tr>
+              </tbody>
+            </table>
+            <p>{description}</p>
+          </div>
+          
 
-          <table className="table table-hover table-bordered single-product-attr">
-            <caption>
-                <button className="btn btn-danger btn-block product-inquiry">Request a Free Quote</button>
-            </caption>
-            <tbody>
-                <tr>
-                  <td>
-                    <h6>Product Model</h6>
-                  </td>
-                  <td className="product-model">
-                    <span className="mm pull-left value">{model}</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>External Dimensions</h6>
-                    <span>(L * W * H)</span>
-                  </td>
-                  <td className="external-dimension">
-                    <span className="mm pull-left value">{external_long} X {external_width} X {external_height}</span>
-                    <span className="pull-right">mm</span>
-                    <hr className="w-100 mt-4 mb-0" />
-                    <span className="inch pull-left value">{(external_long * mmtoinch).toFixed(2)} X {(external_width * mmtoinch).toFixed(2)} X {(external_height * mmtoinch).toFixed(2)}</span>
-                    <span className="pull-right">in</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>Internal Dimensions</h6>
-                    <span>(L * W * H)</span>
-                  </td>
-                  <td className="internal-dimension">
-                    <span className="mm pull-left value">{internal_long} X {internal_width} X {internal_height}</span>
-                    <span className="pull-right">mm</span>
-                    <hr className="w-100 mt-4 mb-0" />
-                    <span className="inch pull-left value">{(internal_long * mmtoinch).toFixed(2)} X {(internal_width * mmtoinch).toFixed(2)} X {(internal_height * mmtoinch).toFixed(2)}</span>
-                    <span className="pull-right">in</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>Folded Height</h6>
-                  </td>
-                  <td className="internal-dimension">
-                    <span className="mm pull-left value">{folded_height}</span>
-                    <span className="pull-right">mm</span>
-                    <hr className="w-100 mt-4 mb-0" />
-                    <span className="inch pull-left value">{(folded_height * mmtoinch).toFixed(2)}</span>
-                    <span className="pull-right">in</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>Weight</h6>
-                  </td>
-                  <td className="internal-dimension">
-                    <span className="kg pull-left value">{weight}</span>
-                    <span className="pull-right">kg</span>
-                    <hr className="w-100 mt-4 mb-0" />
-                    <span className="lbs pull-left value">{(weight * kgtolbs).toFixed(2)}</span>
-                    <span className="pull-right">lbs</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>Volumn</h6>
-                  </td>
-                  <td className="internal-dimension">
-                    <span className="liters pull-left value">{volumn}</span>
-                    <span className="pull-right">Liters</span>
-                    <hr className="w-100 mt-4 mb-0" />
-                    <span className="gallon pull-left value">{(volumn * ltogal).toFixed(2) }</span>
-                    <span className="pull-right">Us gallon</span>
-                  </td>
-                </tr>
-            </tbody>
-          </table>
-          <p>{description}</p>
+          
         </div>
       </div>
 
