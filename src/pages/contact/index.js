d792d70d199db0b42d1615ecfaa25ea093f2470b
Your Name <you@example.com>
2018-11-22 22:09:35 +0800

    back to homepage

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -49,54 +49,56 @@ export default class Index extends React.Component {
                 <li className="breadcrumb-item active" aria-current="page">Contact</li>
               </ol>
             </nav>
-            <div className="content">
-              <h1>Contact</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/?no-cache=1"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field form-group">
-                  <label className="label" htmlFor={"name"} >Your name</label>
-                  <div className="control">
-                    <input className="input form-control" type={"text"} name={"name"} onChange={this.handleChange} id={"name"} required={true} />
+            <div className="content p-3 bg-white">
+              <h1 className="h4">Contact us</h1>
+              <div className="col-md-6">
+                <form
+                  name="contact"
+                  method="post"
+                  action="/contact/thanks/?no-cache=1"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                  onSubmit={this.handleSubmit}
+                >
+                  {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                  <input type="hidden" name="form-name" value="contact" />
+                  <div hidden>
+                    <label>
+                      Don’t fill this out:{" "}
+                      <input name="bot-field" onChange={this.handleChange} />
+                    </label>
                   </div>
-                </div>
-                <div className="field form-group">
-                  <label className="label" htmlFor={"email"}>Email</label>
+                  <div className="field form-group mb-1">
+                    <label className="label" htmlFor={"name"} >Your name</label>
                     <div className="control">
-                      <input className="input form-control" type={"email"} name={"email"} onChange={this.handleChange} id={"email"} required={true} />
+                      <input className="input form-control" type={"text"} name={"name"} onChange={this.handleChange} id={"name"} required={true} />
                     </div>
-                </div>
-                <div className="field form-group">
-                  <label className="label" htmlFor={"p_model"}>Product model</label>
-                    <div className="control">
-                      <input className="input form-control" type={"text"} name={"p_model"} onChange={this.handleChange} id={"p_model"} required={true} />
-                    </div>
-                </div>
-                <div className="field form-group">
-                  <label className="label" htmlFor={"message"}>Message</label>
-                  <div className="control">
-                    <textarea className="textarea form-control" name={"message"} onChange={this.handleChange} id={"email"} required={true} />
                   </div>
-                </div>
-                <div className="field form-group">
-                  <button className="button is-link" type="submit">Send</button>
-                </div>
-              </form>
-        </div>
-        </div>
+                  <div className="field form-group mb-1">
+                    <label className="label" htmlFor={"email"}>Email</label>
+                      <div className="control">
+                        <input className="input form-control" type={"email"} name={"email"} onChange={this.handleChange} id={"email"} required={true} />
+                      </div>
+                  </div>
+                  <div className="field form-group mb-1">
+                    <label className="label" htmlFor={"p_model"}>Product model</label>
+                      <div className="control">
+                        <input className="input form-control" type={"text"} name={"p_model"} onChange={this.handleChange} id={"p_model"} required={true} />
+                      </div>
+                  </div>
+                  <div className="field form-group mb-1">
+                    <label className="label" htmlFor={"message"}>Message</label>
+                    <div className="control">
+                      <textarea className="textarea form-control" name={"message"} onChange={this.handleChange} id={"email"} required={true} />
+                    </div>
+                  </div>
+                  <div className="field form-group mb-1">
+                    <button className="button btn btn-danger is-link" type="submit">Send</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </section>
       </Layout>
     );
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Layout from '../../components/Layout'
+import { navigateTo } from "gatsby-link";
 
 export default () => (
   <Layout>
         <section className="section">
-          <div className="container">
+          <div className="container p-3 mt-3 bg-white">
             <div className="content">
                 <h1>Thank you!</h1>
-                <p>This is a custom thank you page for form submissions</p>
+                <p>We will check the email and come back to you as soon as possible!</p>
+                <button type="button" onClick={()=>navigateTo('/')} class="btn btn-danger">Back To Homepage</button>
             </div>
           </div>
           </section>
