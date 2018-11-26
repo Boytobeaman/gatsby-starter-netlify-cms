b626c4f7089aa59d558b399b466ceadba5bfc4f9
Your Name <you@example.com>
2018-11-26 09:11:51 +0800

    add pic

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,4 @@ export const aliResizeStyle_h_60 = '?x-oss-process=image/resize,h_60';
 export const aliResizeStyle_h_100 = '?x-oss-process=image/resize,h_100';
 export const aliResizeStyle_h_200 = '?x-oss-process=image/resize,h_200';
 export const aliResizeStyle_h_300 = '?x-oss-process=image/resize,h_300';
+export const aliResizeStyle_h_800 = '?x-oss-process=image/resize,h_800';
