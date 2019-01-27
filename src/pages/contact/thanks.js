ae4e0516f8e8a21acdd53b6f606bb1be97a319c7
Your Name <you@example.com>
2019-01-28 02:05:17 +0800

    cc

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -5,13 +5,16 @@ import { navigateTo } from "gatsby-link";
 export default () => (
   <Layout>
         <section className="section">
-          <div className="container p-3 mt-3 bg-white">
-            <div className="content">
-                <h1>Thank you!</h1>
-                <p>We will check the email and come back to you as soon as possible!</p>
-                <button type="button" onClick={()=>window.history.back(2)} class="btn btn-danger mr-3">Back</button>
-                <button type="button" onClick={()=>navigateTo('/')} class="btn btn-danger">Back To Homepage</button>
+          <div className="container-fluid mt-3">
+            <div className="p-3 bg-white">
+              <div className="content">
+                  <h1>Thank you!</h1>
+                  <p>We will check the email and come back to you as soon as possible!</p>
+                  <button type="button" onClick={()=>window.history.go(-2)} class="btn btn-danger mr-3">Back</button>
+                  <button type="button" onClick={()=>navigateTo('/')} class="btn btn-danger">Back To Homepage</button>
+              </div>
             </div>
+            
           </div>
           </section>
   </Layout>
