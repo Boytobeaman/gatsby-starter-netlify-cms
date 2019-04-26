cf4b55c327c058e3a4e0b7affbd26836d2ffbc40
Your Name <you@example.com>
2019-04-26 08:32:00 +0800

    change homepage pic to cdn url

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Slider from '../components/Slider'
+import { cdn_url } from '../utils'
 export default class IndexPage extends React.Component {
   render() {
 
@@ -28,20 +29,20 @@ export default class IndexPage extends React.Component {
                     <h6>JOINPLASTIC provide all kinds of plastic products,including:</h6>
                     <ul className="nav flex-column" id="homePageCatList">
                       <li className="nav-item">
-                        <a className="nav-link text-danger font-weight-bold pb-0" href="#"><img className="" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/plastic-moving-crates.jpg"
+                        <a className="nav-link text-danger font-weight-bold pb-0" href="#"><img className="" src={`${cdn_url}/static/plastic-moving-crates.jpg`}
                             alt="" /> Plastic Moving Crates</a>
                       </li>
                       <li className="nav-item"><a className="nav-link text-danger font-weight-bold pb-0" href="#">
-                          <img className="" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/euro-stacking-crates.jpg"
-                            alt="" /> Euro Stacking Crates</a>
+                          <img className="" src={`${cdn_url}/static/euro-stacking-crates.jpg`}
+                            alt="euro stacking crates" /> Euro Stacking Crates</a>
                       </li>
                       <li className="nav-item"><a className="nav-link text-danger font-weight-bold pb-0" href="#">
-                          <img className="" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/folding-crates.jpg"
-                            alt="" /> Folding Crates</a>
+                          <img className="" src={`${cdn_url}/static/folding-crates.jpg`}
+                            alt="Folding Crates" /> Folding Crates</a>
                       </li>
                       <li className="nav-item"><a className="nav-link text-danger font-weight-bold pb-0" href="#">
-                          <img className="" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/plastic-pallet-boxes.jpg"
-                            alt="" /> Plastic Pallet Boxes</a>
+                          <img className="" src={`${cdn_url}/static/plastic-pallet-boxes.jpg`}
+                            alt="Plastic Pallet Boxes" /> Plastic Pallet Boxes</a>
                       </li>
           
                     </ul>
@@ -51,7 +52,7 @@ export default class IndexPage extends React.Component {
                 </div>
               </div>
               <div className="col-sm-5 pr-0">
-                <img src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/plastic-crate-manufacture-factory.jpg" className="img-fluid"
+                <img src={`${cdn_url}/static/plastic-crate-manufacture-factory.jpg`} className="img-fluid"
                   alt="" />
               </div>
             </div>
@@ -61,17 +62,17 @@ export default class IndexPage extends React.Component {
               <div className="row text-center">
                   <div className="col-sm-12 col-md-4">
                     <h5 className="text-danger">Moving company</h5>
-                    <img className="img-fluid px-4" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/moving-company.jpg" alt="" />
+                    <img className="img-fluid px-4" src={`${cdn_url}/static/moving-company.jpg`} alt="" />
                     <p className="p-4">Moving isn’t always great for the environment. Luckily, we provide <strong>plastic moving boxes</strong> for moving companies to make moving more planet-friendly.</p>
                   </div>
                   <div className="ol-sm-12 col-md-4">
                     <h5 className="text-danger">Retailers</h5>
-                    <img className="img-fluid px-4" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/retails.jpg" alt="" />
+                    <img className="img-fluid px-4" src={`${cdn_url}/static/retails.jpg`} alt="" />
                     <p className="p-4">Retailers can enjoy the high quality and competitive price of our plastic products,and we had build a good relationship with retails across the world</p>
                   </div>
                   <div className="ol-sm-12 col-md-4">
                     <h5 className="text-danger">Anyone</h5>
-                    <img className="img-fluid px-4" src="https://lu-ali-us-west.oss-us-west-1.aliyuncs.com/static/anyboy.jpg" alt="" />
+                    <img className="img-fluid px-4" src={`${cdn_url}/static/anyboy.jpg`} alt="" />
                     <p className="p-4">We had supplied <strong>plastic crates</strong> for farms, schools, corporations and organizations.And we provide good Pre-and post-sales support for our customers</p>
                   </div>
                 </div>
