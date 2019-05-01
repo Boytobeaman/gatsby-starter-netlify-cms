387a52946bc563c7f4a79dcd31f3e8dcc633a5fe
Your Name <you@example.com>
2019-05-01 13:52:09 +0800

    move product detail of cat to a component

--- src/pages/folding-crates/index.js
+++ src/pages/folding-crates/index.js
@@ -1,37 +1,13 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { navigateTo } from "gatsby-link";
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import ProductDetailTemplateCat from '../../components/ProductDetailTemplateCat'
 import Layout from '../../components/Layout'
-import {
-  mmtoinch,
-  kgtolbs,
-  ltogal,
-  aliResizeStyle_h_20,
-  aliResizeStyle_h_200,
-  aliResizeStyle_h_300,
-  aliResizeStyle_w_400,
-  aliResizeStyle_w_600,
-  aliResizeStyle_w_800,
-  aliResizeStyle_w_900,
-  aliResizeStyle_w_1024,
-  aliResizeStyle_w_1200,
-  contact_url
-} from '../../utils';
 import '../../cat-page-style.scss'
 export default class FoldingcratePage extends React.Component {
   constructor(props){
     super(props);
-    this.toContactUs = this.toContactUs.bind(this);
-  }
-  toContactUs(e){
-      e.preventDefault();
-      navigateTo(contact_url)
-      return false
-  
   }
   render() {
     const { data } = this.props
@@ -58,36 +34,6 @@ export default class FoldingcratePage extends React.Component {
             
             {posts
               .map(({ node: post }) => {
-                const {
-                  title,
-                  model,
-                  external_long,
-                  external_width,
-                  external_height,
-                  internal_long,
-                  internal_width,
-                  internal_height,
-                  folded_height,
-                  volumn,
-                  weight,
-                  images
-                } = post.frontmatter;
-                let cat_image_url=''
-                let srcset=''
-                let placeholderImg=''
-                if (images && images.length > 0 ) {
-                  let the_image = post.frontmatter.images[0].replace("http:","https:")
-                  cat_image_url = the_image + aliResizeStyle_h_200
-                  placeholderImg = the_image + aliResizeStyle_h_20
-                  srcset=`
-                    ${the_image}${aliResizeStyle_h_300} 300w, 
-                    ${the_image}${aliResizeStyle_w_400} 400w, 
-                    ${the_image}${aliResizeStyle_w_600} 600w,
-                    ${the_image}${aliResizeStyle_w_800} 800w,
-                    ${the_image}${aliResizeStyle_w_900} 900w,
-                    ${the_image}${aliResizeStyle_w_1024} 1024w,
-                    ${the_image}${aliResizeStyle_w_1200} 1200w`
-                }
                 return (
                   <div
                     className="clearfix mb-3"
@@ -95,83 +41,9 @@ export default class FoldingcratePage extends React.Component {
                   >
                     <div className="col-xs-12">
                       <Link className="cat-product-link" to={post.fields.slug}>
-                        <div className="product-wrap">
-                          <div className="product-img-wrap">
-                            <LazyLoadImage 
-                              src={cat_image_url} 
-                              placeholderSrc={placeholderImg}
-                              effect="blur"
-                              srcset={srcset} 
-                              sizes="(max-width: 300px) 100vw, 300px"
-                              className="" 
-                              alt={post.frontmatter.title} />
-                          </div>
-                          <div className="product-right">
-                            <div className="product-name">
-                              <div className="col-sm-12 py-1 clearfix">
-                                <h2 title={title} className="product-title text-capitalize text-truncate d-inline-block mb-0 pl-1">{title}</h2>
-                                <span className="btn btn-danger pull-right float-right product-cat-inquiry" onClick={this.toContactUs}>Inquiry</span>
-                                <span className="badge badge-info product-model mr-3">Model: {model}</span>
-                              </div>
-                            </div>
-                            <div className="product-attributes">
-                              <div className="row no-gutters">
-                              <div className="col-sm-7">
-                                <div className="row no-gutters">
-                                  <div className="col-sm-6 border-right border-white external-dimension">
-                                    <div className="table-head bb-2-white">External Dimensions</div>
-                                    <div className="product-val-mm">
-                                      <span className="value">{external_long} X {external_width} X {external_height}</span>
-                                      <span className="pull-right float-right">mm</span>
-                                    </div>
-                                    <div className="product-val-inch">
-                                      <span className="value">{(external_long * mmtoinch).toFixed(2)} X {(external_width * mmtoinch).toFixed(2)} X {(external_height * mmtoinch).toFixed(2)}</span>
-                                      <span className="pull-right float-right">in</span>
-                                    </div>
-                                  </div>
-                                  <div className="col-sm-6 border-right border-white internal-dimension d-none d-sm-block">
-                                    <div className="table-head bb-2-white">Internal Dimensions</div>
-                                    <div className="product-val-mm">
-                                      <span className="value">{internal_long} X {internal_width} X {internal_height}</span>
-                                      <span className="pull-right float-right">mm</span>
-                                    </div>
-                                    <div className="product-val-inch">
-                                      <span className="value">{(internal_long * mmtoinch).toFixed(2)} X {(internal_width * mmtoinch).toFixed(2)} X {(internal_height * mmtoinch).toFixed(2)}</span>
-                                      <span className="pull-right float-right">in</span>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="col-sm-5 d-none d-sm-block">
-                                <div className="row no-gutters">
-                                  <div className="col-sm-6 border-right border-white weight">
-                                    <div className="table-head bb-2-white">Weight</div>
-                                    <div className="product-val-mm">
-                                      <span className="value">{weight}</span>
-                                      <span className="pull-right float-right">kg</span>
-                                    </div>
-                                    <div className="product-val-inch">
-                                      <span className="value">{(weight * kgtolbs).toFixed(2)}</span>
-                                      <span className="pull-right float-right">lbs</span>
-                                    </div>
-                                  </div>
-                                  <div className="col-sm-6 volumn">
-                                    <div className="table-head bb-2-white">Volume</div>
-                                    <div className="product-val-mm">
-                                      <span className="value">{volumn}</span>
-                                      <span className="pull-right float-right">Liters</span>
-                                    </div>
-                                    <div className="product-val-inch">
-                                      <span className="value">{(volumn * ltogal).toFixed(2)}</span>
-                                      <span className="pull-right float-right">Us gallon</span>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                        <ProductDetailTemplateCat 
+                          data={post.frontmatter}
+                        />
                       </Link>
                     </div>
                   </div>
