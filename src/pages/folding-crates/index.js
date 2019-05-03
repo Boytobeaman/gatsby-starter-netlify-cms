ef6610b1e5a80c83421d9f03808d2f0335a45827
Your Name <you@example.com>
2019-05-04 07:12:50 +0800

    b

--- src/pages/folding-crates/index.js
+++ src/pages/folding-crates/index.js
@@ -39,7 +39,6 @@ export default class FoldingcratePage extends React.Component {
                   <source src={`${cdn_url}/static/video/collapsible-crate.mp4`} type="video/mp4" />
                   Your browser does not support HTML5 video.
                 </video>
-                <div className="bg-white text-center">video show for the collapsible crate</div>
               </div>
               <div className="col-sm-4">
                 <LazyLoadImage 
