cece448e8e55902eef3bd654dbf92c4224cd1ddd
Your Name <you@example.com>
2019-05-03 16:17:24 +0800

    add site url for xml site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby + Netlify CMS Starter',
+    siteUrl: `https://www.movingboxsale.com`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
