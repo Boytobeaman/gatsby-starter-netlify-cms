8f2446befc8d2e77836f1aeb10f7544a0fc62667
Your Name <you@example.com>
2019-05-03 09:22:14 +0800

    helmet

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
 import Layout from '../../components/Layout'
 import Slider from '../../components/Slider'
 import { cdn_url } from '../../utils'
