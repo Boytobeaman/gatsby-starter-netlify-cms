30c6168b32a17e055cab8eddb42c457aed7835f3
Your Name <you@example.com>
2019-05-05 13:14:57 +0800

    add favcon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,17 @@ import Helmet from 'react-helmet'
 
 import MainNavbar from '../components/MainNavbar'
 import Footer from './Footer'
+import favicon from '../img/movingboxlogo.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './all.scss'
 
 
 const TemplateWrapper = ({ children }) => (
   <div className="position-relative" style={{paddingBottom: '55px'}}>
-    <Helmet title="Home | Moving Crates Supplier" />
+    <Helmet>
+      <title>Home | Moving Crates Supplier</title>
+      <link rel="icon" type="image/svg+xml" href={favicon}></link>
+    </Helmet>
     <MainNavbar />
     <div>{children}</div>
     <Footer />
