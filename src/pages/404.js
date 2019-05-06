d8d2e6a400608a26256dfc23fb4eb58dca2db999
Your Name <you@example.com>
2019-05-06 20:52:30 +0800

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container mt-5">
+      <div className="text-center">
+        <div className='rounded bg-white  p-3'>
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <Link className="btn btn-danger btn-lg active" role="button" aria-pressed="true" to="/">
+            Back to homepage
+          </Link>
+        </div>
+      </div>
     </div>
+    
   </Layout>
 )
 
