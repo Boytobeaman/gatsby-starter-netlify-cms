e0823c8a3f8a69bc8f19d403281f4efb04e4d133
Your Name <you@example.com>
2019-05-29 18:10:44 +0800

    add navbar rich data

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -28,7 +28,7 @@ export default class MainNavbar extends React.Component{
   }
   render() {
     return (
-      <Navbar color="dark" dark expand="md" id="mainNavbar">
+      <Navbar color="dark" dark expand="md" id="mainNavbar" itemtype="http://schema.org/SiteNavigationElement">
         <NavbarBrand className="py-0">
           <Link to="/" className="navbar-brand py-0">
                 <figure className="image mb-0">
